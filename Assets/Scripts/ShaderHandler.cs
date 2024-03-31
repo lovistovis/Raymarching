06b23bf777479d145dc407c9daee8143bcfe9be3
Love Lysell Berglund <love.lysellberglund@learnet.se>
2024-03-31 20:57:47 +0200

    Additional tools + cool setup 2

--- Assets/Scripts/ShaderHandler.cs
+++ Assets/Scripts/ShaderHandler.cs
@@ -1,23 +1,28 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 public class ShaderHandler : MonoBehaviour
 {
     [SerializeField] private ComputeShader rayMarchingShader;
-    [SerializeField] private float walkSpeed;
-    [SerializeField] private float runSpeed;
-    [SerializeField][Range(1, 10000)] private int rayMarchingIterations;
+    [SerializeField] private Vector3 startPos = new Vector3(2.0f, 1.0f, 0.5f);
+    [SerializeField] private float baseMoveSpeed;
+    [FormerlySerializedAs("rayMarchingIterations")] [SerializeField][Range(1, 10000)] private int baseRayMarchingIterations;
     [SerializeField][Range(0, 2)] private float mouseSensitivity;
     [SerializeField][Range(0, 10)] private float zoomSensitivity;
+    [SerializeField][Range(0, 10)] private float speedSensitivity;
+    [SerializeField][Range(0, 1)] private float speedFactorOnShift;
 
     private RenderTexture renderTexture;
-    private Vector3 position = new Vector3(2.0f, 1.0f, 0.5f);
+    private Vector3 position;
     private Quaternion cameraRotation = Quaternion.identity;
     private Vector3 cameraDirection = new Vector3(0, 0, 0);
     private Camera mainCamera;
 
     private float cameraRotationX = 0;
     private float cameraRotationY = 0;
+    private float moveSpeed;
+    private int iterations;
 
     private float lastTime = 0;
 
@@ -32,6 +37,10 @@ public class ShaderHandler : MonoBehaviour
         // Set up render texture
         renderTexture = new RenderTexture(Screen.width, Screen.height, 24);
         renderTexture.enableRandomWrite = true;
+
+        position = startPos;
+        moveSpeed = baseMoveSpeed;
+        iterations = baseRayMarchingIterations;
 
         // Lock and hide cursor
         Cursor.lockState = CursorLockMode.Locked;
@@ -50,21 +59,40 @@ public class ShaderHandler : MonoBehaviour
 
 
         // Movement
-        float moveSpeed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
         //float moveSpeed = walkSpeed + Time.timeSinceLevelLoad / 100;
 
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            moveSpeed *= Mathf.Abs(1 + scroll * speedSensitivity);
+        }
+        else if (Input.GetKey(KeyCode.I))
+        {
+            iterations += scroll != 0 ? Mathf.RoundToInt(Mathf.Sign(scroll)) : 0;
+            iterations = Mathf.RoundToInt(iterations * Mathf.Abs(1 + scroll * speedSensitivity));
+        }
+        else
+        {
+            mainCamera.fieldOfView -= scroll * zoomSensitivity;
+            mainCamera.fieldOfView = Mathf.Clamp(mainCamera.fieldOfView, 0, 180);
+        }
+
         //position += transform.forward * moveSpeed;
+        float currentMoveSpeed = Input.GetKey(KeyCode.LeftShift) ? moveSpeed * speedFactorOnShift : moveSpeed;
         position += (transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical")) * moveSpeed;
         
         if (Input.GetKey(KeyCode.LeftControl)) { lastTime += Time.deltaTime; }
         else if (Input.GetKey(KeyCode.LeftAlt)) { lastTime -= Time.deltaTime; }
 
-        mainCamera.fieldOfView -= Input.GetAxis("Mouse ScrollWheel") * zoomSensitivity;
-        mainCamera.fieldOfView = Mathf.Clamp(mainCamera.fieldOfView, 0, 180);
+        if (Input.GetKeyDown(KeyCode.R)) {
+            mainCamera.fieldOfView = 60;
+            moveSpeed = baseMoveSpeed;
+        }
+        
+        if (Input.GetKey(KeyCode.M)) { position = startPos; }
 
-        if (Input.GetKeyDown(KeyCode.R)) { mainCamera.fieldOfView = 60; }
-
-        Debug.Log((int)(Time.timeSinceLevelLoad * 100));
+        Debug.Log((Time.timeSinceLevelLoad * 100).ToString() + ", " + iterations.ToString());
     }
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
@@ -77,7 +105,7 @@ public class ShaderHandler : MonoBehaviour
         rayMarchingShader.SetTexture(kernelIndex, "RenderTexture", renderTexture);
         rayMarchingShader.SetBuffer(kernelIndex, "LoseBuffer", loseBuffer);
         rayMarchingShader.SetInts("Resolution", renderTexture.width, renderTexture.height);
-        rayMarchingShader.SetInt("RayMarchingIterations", rayMarchingIterations);
+        rayMarchingShader.SetInt("RayMarchingIterations", iterations);
         rayMarchingShader.SetFloats("CameraPosition", position.x, position.y, position.z);
         rayMarchingShader.SetFloat("Time", lastTime);
         rayMarchingShader.SetMatrix("CameraToWorld", mainCamera.cameraToWorldMatrix);
